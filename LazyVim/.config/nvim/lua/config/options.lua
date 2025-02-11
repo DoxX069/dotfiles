d21b161baaa8f9437489ebc5f41458c36681ec71
DoxX069 <ammonnils@gmail.com>
2025-02-11 12:24:32 +0100

    Lazyvim update

--- LazyVim/.config/nvim/lua/config/options.lua
+++ LazyVim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = false -- Disable relative line numbers
+vim.opt.wrap = true -- Set wrap on
+vim.opt.expandtab = false -- Convert tabs to spaces
+vim.opt.cursorline = false -- Do not highlight current line
